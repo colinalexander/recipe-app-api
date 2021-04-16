26b4c1e4d9b3864c36be26aee59984b3dc7874db
Colin Alexander <colin.1.alexander@gmail.com>
2021-04-15 19:42:28 -0700

    fixed flake8 max col width issue

--- app/app/settings.py
+++ app/app/settings.py
@@ -84,18 +84,19 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
+base = 'django.contrib.auth.password_validation.''
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': base + 'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': base + 'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': base + 'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': base + 'NumericPasswordValidator',
     },
 ]
 
